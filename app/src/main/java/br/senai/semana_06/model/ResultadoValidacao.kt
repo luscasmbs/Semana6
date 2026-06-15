f360ccf1957226a6dc9310ed1337f8717b613e5a
luscasmbs <lucasmiguelbsilva@gmail.com>
2026-06-15 17:07:35 -0300

    feat: validação

---
+++ app/src/main/java/br/senai/semana_06/model/ResultadoValidacao.kt
@@ -0,0 +1,33 @@
+package br.senai.semana_06.model
+
+sealed class ResultadoValidacao{
+    object Valido: ResultadoValidacao()
+    data class Invalido(val mensagens: List<String>):
+            ResultadoValidacao()
+}
+data class Entrega(
+    val readme: Boolean,
+    val commits: Int,
+    val checklist: Boolean
+)
+fun ValidarEntrega(entrega: Entrega): ResultadoValidacao{
+    val erros = mutableListOf<String>()
+
+    if (!entrega.readme){
+        erros.add("É obrigatório ter um readme")
+    }
+    if (entrega.commits < 3) {
+        erros.add("É obrigatório no mínimo 3 commits")
+    }
+
+    if (!entrega.checklist) {
+        erros.add("Checklist é obrigatório")
+    }
+
+    return if (erros.isEmpty()) {
+        ResultadoValidacao.Valido
+    } else {
+        ResultadoValidacao.Invalido(erros)
+    }
+
+}
